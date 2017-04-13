23b209c8e1e2eb3b89ee89d9b27d933d65e2cac8
Prateek Batra <prateekbatra54@gmail.com>
2017-04-13 19:56:23 +0530

    SOS
    side bar menu for mobile version on clik not working

---
+++ script/javas.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidenavbar").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenavbar").style.width = "0";
+}
